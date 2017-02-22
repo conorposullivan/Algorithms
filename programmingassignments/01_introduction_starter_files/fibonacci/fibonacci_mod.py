8baa3d9eab78e8d48c3ea6dc8e3e65085447cca0
Conor <conorposullivan@gmail.com>
2017-02-22 20:42:27 +0000

    first commit

---
+++ programmingassignments/01_introduction_starter_files/fibonacci/fibonacci_mod.py
@@ -0,0 +1,12 @@
+# Uses python3
+def calc_fib(n):
+    fib_lst = []
+    fib_lst.append(0)
+    fib_lst.append(1)
+    for i in range(2,n+1):
+        i_num = (fib_lst[i-1] + fib_lst[i-2]) % 10
+        fib_lst.append(i_num)
+    return fib_lst[n]
+
+n = int(input())
+print(calc_fib(n))
---
+++ programmingassignments/01_introduction_starter_files/gcd/gcd.py
@@ -0,0 +1,13 @@
+# Uses python3
+import sys
+
+def gcd_euclid(a, b):
+    if b==0:
+        return a
+    a_prime = a % b
+    return gcd_euclid(b,a_prime)
+
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    a, b = map(int, input.split())
+    print(gcd_euclid(a, b))
